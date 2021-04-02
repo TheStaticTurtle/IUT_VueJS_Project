647e4fc7751d07251f980cc1f4b96d7616032556
TheStaticTurtle <tuglersamuel@gmail.com>
2021-04-02 04:56:28 +0200

    Edited backstory

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
-	<v-container>
+	<v-container >
 		<h1>Backstory</h1>
 		<p>A long time ago was born James Gosling, after designing his first weapon of mass destruction called Java he then proceeded to change his research field</p>
-		<p>A few years back he started to take interest in micro biology and infectius deseases. After a bit of time he became known as Dr Mad</p>
+		<p>A few years back he started to take interest in micro biology and infectious deceases. After a bit of time he became known as Dr Mad</p>
 		<p>You have now entered his laboratory but it's password protected, will you be able to find the username and password?</p>
-		<p>If you don't, James will be able to create the most monstruous viruses ever known and you will let him finnaly destroy the world for good</p>
+		<p>If you don't, James will be able to create the most monstrous viruses ever known and you will let him finally destroy the world for good (Don't let anyone know but I heard he teamed up with Fabien Potencier, you know the creator of Symfony)</p>
 		<p>Good luck ;)</p>
 	</v-container>
 </template>
